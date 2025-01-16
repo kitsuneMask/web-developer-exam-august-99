23d37aa79dead757274065f0bb65a8c9614e8904
RYM <ryeses14@gmail.com>
2025-01-16 11:50:13 +0800

    Setup Finished

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+
+    <title>Book Management System</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./source/css/index.css" />
+
+</head>
+<body>
+    <main>
+        <button>Add</button>
+        <div class="table-con">
+
+            <div>
+                <h3 class="fw-bold">TITLE</h3>
+                <div></div>
+            </div>
+
+            <div>
+                <h3 class="fw-bold">ISBN</h3>
+                <div></div>
+            </div>
+
+            <div>
+                <h3 class="fw-bold">AUTHOR</h3>
+                <div></div>
+            </div>
+
+            <div>
+                <h3 class="fw-bold">PUBLISHER</h3>
+                <div></div>
+            </div>
+
+            <div>
+                <h3 class="fw-bold">YEAR PUBLISHED</h3>
+                <div></div>
+            </div>
+
+            <div>
+                <h3 class="fw-bold">CATEGORY</h3>
+                <div></div>
+            </div>
+
+            <div>
+                <span></span>
+                <div></div>
+            </div>
+
+        </div>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
